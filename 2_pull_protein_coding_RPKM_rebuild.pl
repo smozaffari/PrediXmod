2241d869878bc245172da357273ce499376bfb71
hwheeler01 <hwheeler@bsd.uchicago.edu>
2014-12-08 15:07:44 -0600

    added input/output info

--- 2_pull_protein_coding_RPKM_rebuild.pl
+++ 2_pull_protein_coding_RPKM_rebuild.pl
@@ -3,13 +3,23 @@ use strict;
 use warnings;
 use Compress::Zlib;
 
+
+####This perl script takes the GTEx RNA-Seq *gene_rpkm.gxt.gz file as input, extracts protein coding genes,
+#### and makes several output files:
+#### .gene_rpkm.protein-coding.gct
+#### .GENExID matrix for quick scanning into R
+#### .ID.list colnames of matrix
+#### .GENE.list rownames of matrix (Ensembl ID)
+#### .GENEname.list matching gene names for Ensembl IDs
+
+
 my $gtexdir = "/nas40t2/gtex/GTEx_Analysis_2014-06-13/rna-seq/";
 my $file = "GTEx_Analysis_2014-06-13_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct.gz";
 
 my $dir = "/nas40t2/hwheeler/PrediXcan_CV/GTEx_2014-06013_release/";
 
 my $pcfile = ${dir} . "gencode.v18.genes.patched_contigs.summary.protein";
-open(PCGENES, $pcfile); ##from /nas40t2/gtex/GTEx_Analysis_2014-06-13/reference_files/gencode.v18.genes.patched_contigs.gtf.gz
+open(PCGENES, $pcfile); ##from /nas40t2/gtex/GTEx_Analysis_2014-06-13/reference_files/gencode.v18.genes.patched_contigs.gtf.gz, grep protein_coding
 
 my %pcgenes;
 while(<PCGENES>){
