ff4fe1131431c4b29ce810680e24a88cf543bfa0
hwheeler01 <hwheeler@bsd.uchicago.edu>
2015-02-09 14:53:54 -0600

    add change permissions to run scripts

--- DGN-WB/DGN-calc-weights/make_run_scripts_01.pl
+++ DGN-WB/DGN-calc-weights/make_run_scripts_01.pl
@@ -10,6 +10,6 @@ for(my $i = 1; $i<=22; $i++){
 	my $outfile = "runscripts/run_" . $prescript . "_chr" . $i . "_alpha" . $k . ".sh";
 	print QSUB "qsub $outfile\nsleep 4\n";
 	open(OUT, ">$outfile");
-	print OUT "#!/bin/bash\n#PBS -N R.glmnet\n#PBS -S /bin/bash\n#PBS -l walltime=72:00:00\n#PBS -l nodes=1:ppn=1\n#PBS -l mem=10gb\n#PBS -o logs/\$\{PBS_JOBNAME\}.o\$\{PBS_JOBID\}.log\n#PBS -e logs/\$\{PBS_JOBNAME\}.e\$\{PBS_JOBID\}.err\n\ncd \$PBS_O_WORKDIR\nmodule load R\n\ntime R --no-save < $prescript.r --args $i $k\n";
+	print OUT "#!/bin/bash\n#PBS -N R.glmnet\n#PBS -S /bin/bash\n#PBS -l walltime=72:00:00\n#PBS -l nodes=1:ppn=1\n#PBS -l mem=10gb\n#PBS -o logs/\$\{PBS_JOBNAME\}.o\$\{PBS_JOBID\}.log\n#PBS -e logs/\$\{PBS_JOBNAME\}.e\$\{PBS_JOBID\}.err\n\ncd \$PBS_O_WORKDIR\nmodule load R\n\ntime R --no-save < $prescript.r --args $i $k\nchmod 750 *\n";
     }
 }
